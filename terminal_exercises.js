e0c8561b900f7e2638563e5e5c730a89fe4b9ebc
Julia-Ries <113562919+Julia-Ries@users.noreply.github.com>
2022-11-03 13:17:21 -0400

    Add files via upload

---
+++ terminal_exercises.js
@@ -0,0 +1,26 @@
+// Part I
+
+// 1. mkdir first
+// 2. cd first
+// 3. touch person.txt
+// 4. mv person.txt another.txt 
+// 5. cp another.txt copy.txt
+// 6. rm copy.tx
+// 7. cp -r first second
+// 8. rm -rf second
+
+
+// Part II
+
+// 1. man is the manual for that command and shows you all of the flags. Q to exit man pages
+// 2. -l lists files in long format -a lists everythign that begins with a . (hidden stuff)
+// 3. press option and left/right arrows
+// 4. press control + e
+// 5.  press control + a 
+// 6. press option + delete
+// 7. terminal is the application that gives us comand line interface while shell is the program that processes the commands and returns output
+//    termianl is hte wrapper program which runs the shell.
+// 8. absolute path is starting from the very beginning and gives complete location starting with root directory
+// 9. relative path is relative to where i am currently and starts at the current location
+// 10. a flag is changes the commands. ive used -a to list all files including hidden ones, -l for complete info, cp -r to copy directory
+// 11. the r flag removes folders and -f flag removes a folder with force removal
